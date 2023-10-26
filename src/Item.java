a6233c179cdef5f44fbc5da4866a0c3f43bcf28f
rudykreutzer <kreutzerrudy@gamil.com>
2023-10-26 13:29:42 -0500

    Start

--- src/Item.java
+++ src/Item.java
@@ -1,12 +1,32 @@
 public class Item {
-    private String name;
+    private final String name;
     private int amount;
-    public Item(String inName, int inAmount){
-        name=inName;
-        amount=inAmount;
+
+    public Item(String inName, int inAmount) {
+        name = inName;
+        amount = inAmount;
+    }
+    //add and subtract are methods used to alter quantity
+    public void add(int quantity) {
+        amount += quantity;
     }
 
-    public String toString(){
-        return String.format("%s: %d",name,amount);
+    public void subtract(int quantity) {
+        if (quantity >= amount) {
+            amount = 0;
+        } else amount -= quantity;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String toString() {
+        return String.format("%s: %d\n", name, amount);
     }
 }
---
+++ src/list.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class list {
+    public String name;
+    ArrayList<Item> items = new ArrayList<>();
+
+    public list(String name) {
+        this.name = name;
+    }
+    //takes a string and returns the index of a item in the array with the same name
+    public int findName(String name) {
+        for (int i = 0; i < items.size(); ++i) {
+            if (items.get(i).getName().equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    //uses findName to index an item of "named" and increases it by amount
+    public void increase(String name, int amount) throws IndexOutOfBoundsException {
+        int index = findName(name);
+        if (index == -1) {
+            items.add(new Item(name, amount));
+        } else items.get(index).add(amount);
+    }
+    //same as increase but subtracts and prints an error message when you subtract more than the amount if items
+    public void decrease(String name, int amount) throws IndexOutOfBoundsException {
+        int index = findName(name);
+        int original = items.get(index).getAmount();
+        items.get(index).subtract(amount);
+        if (items.get(index).getAmount() < 1) {
+            System.out.printf("Warning removing quantity of %d while %s only " +
+                    "has quantity of %d\n", amount, name, original);
+            items.remove(index);
+        }
+    }
+
+    public String toString() {
+        String string = String.format("%s:\n", name);
+
+        for (Item item : items) {
+            string += item;
+        }
+
+        return string;
+    }
+}
